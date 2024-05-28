d90b7806ce393428be17a4bc3dd1b0f198854617
rahimkolachi07 <raheemkolachi7@gmail.com>
2024-05-28 20:15:40 +0500

    updated

--- api.py
+++ api.py
@@ -71,4 +71,4 @@ async def echo(text_in: TextIn):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=5000)
